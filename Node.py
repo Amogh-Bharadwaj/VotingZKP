fabb4874f92d858233c003b9dd9ffbf12a913168
shrikrishnalolla <52192798+shrikrishnalolla@users.noreply.github.com>
2022-04-27 18:08:10 +0530

    Added Comments

--- Node.py
+++ Node.py
@@ -4,20 +4,24 @@ from random import randint
 # A class for node (a voter/candidate in our case), with ZKP prover.
 class Node:
     def __init__(self,weight:int):
-        self.secret = randint(1,10000000)
-        self.publicKnowledge = pow(G,self.secret,P)
+        self.secret = randint(1,10000000) #private key of the node
+        self.publicKnowledge = pow(G,self.secret,P) #power(G,secret) (mod P); public key
+        # G -> Generator, P -> Prime number, 
+        # brute method of solving for x -> Disc log
         self.r = None
-
+        # r -> random number belonging to [0,p-1]
         self.votingWeight = weight
-        self.votes = 0 # Relevant only for candidates
+        self.votes = 0 # Relevant only for candidates, number of votes a candidate has got
     
     def Prove(self,questionNumber,verifierData):
         if questionNumber==0:
+            #first step of ZKP, h = power(G,r) (mod p)
             self.r = randint(0, P-1)
             h = pow(G,self.r,P)
             return h
 
         else:
+            #third step of ZKP:
             b = verifierData
             x = self.secret
             s = (self.r + b*x) % (P-1)
--- Transaction.py
+++ Transaction.py
@@ -6,13 +6,14 @@ import time
 # A transaction class with ZKP verifier.
 class Transaction:
     def __init__(self, voter:Node,candidate):
-        self.voter = voter
-        self.candidate = candidate
-        self.timestamp = time.time()
+        self.voter = voter #the node
+        self.candidate = candidate #which candidate the voter chose
+        self.timestamp = time.time() #time stamp
 
     def ZKP_TransactionVerification(self):
         
         ROUNDS = 500
+        #Multiple rounds to ensure that the 
         for Round in range(ROUNDS):
 
             y = self.voter.publicKnowledge
@@ -20,11 +21,13 @@ class Transaction:
             # Node: Chooses 0 <= r < p - 1, sends h = pow(g,r,p)
             h = self.voter.Prove(0, None)
 
+            # Second Step of ZKP:
             # Miner: Gives b = randint(0,1)
             # Node: Sends s = r+b*x % p-1
             b = randint(0,1)
             s = self.voter.Prove(1, b)
 
+            # Step 4: verify
             # Miner: Checks if pow(g,s,p) == h*pow(y,b,p) % p
             valid = pow(G,s,P) == (h*pow(y,b,P)) % P
             
